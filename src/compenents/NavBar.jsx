2ad28b02b237054a4ba4035467b88f347517485d
Ibrahim <ydngreg@gmail.com>
2024-04-15 15:06:45 +0200

    Ajout navbar.MAP

--- src/compenents/NavBar.jsx
+++ src/compenents/NavBar.jsx
@@ -3,18 +3,15 @@ import React from 'react';
 function NavBar({ pokemonIndex, setPokemonIndex, pokemonList }) {
   return (
     <div className="nav-bar">
-      <button
-        onClick={() => setPokemonIndex((prevIndex) => Math.max(prevIndex - 1, 0))}
-        disabled={pokemonIndex === 0}
-      >
-        Précédent
-      </button>
-      <button
-        onClick={() => setPokemonIndex((prevIndex) => Math.min(prevIndex + 1, pokemonList.length - 1))}
-        disabled={pokemonIndex === pokemonList.length - 1}
-      >
-        Suivant
-      </button>
+      {pokemonList.map((pokemon, index) => (
+        <button
+          key={index}
+          onClick={() => setPokemonIndex(index)}
+          className={index === pokemonIndex ? 'active' : ''}
+        >
+          {pokemon.name}
+        </button>
+      ))}
     </div>
   );
 }
